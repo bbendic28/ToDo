1f11e30c7eba6c495ccf9865505bd3a3f78392e5
Cubes School 2 <bbendic@yahoo.co.uk>
2016-03-30 14:43:13 +0200

    moji komenatri

---
+++ ToDo/ToDo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToDo
+//
+//  Created by Cubes School 2 on 3/28/16.
+//  Copyright © 2016 Cubes School 2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@end
+
---
+++ ToDo/ToDo/ViewController.m
@@ -0,0 +1,16 @@
+//
+//  ViewController.m
+//  ToDo
+//
+//  Created by Cubes School 2 on 3/28/16.
+//  Copyright © 2016 Cubes School 2. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@end
