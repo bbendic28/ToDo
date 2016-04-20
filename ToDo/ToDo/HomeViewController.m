827552fe1ca1ba175bc3947cdbb8d4199fd6a722
Cubes School 2 <bbendic@yahoo.co.uk>
2016-04-20 16:04:04 +0200

    camera

--- ToDo/ToDo/HomeViewController.m
+++ ToDo/ToDo/HomeViewController.m
@@ -10,11 +10,27 @@
 #import "TaskTableViewCell.h"
 #import "Constants.h"
 
-@interface HomeViewController () <UITableViewDataSource,UITableViewDelegate>
+@interface HomeViewController () <UITableViewDataSource,UITableViewDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 
 @end
 
 @implementation HomeViewController
+
+#pragma mark - View lifecyle
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+    UITapGestureRecognizer *tap= [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                         action:@selector(pickImage)];
+    tap.numberOfTapsRequired=1;
+    
+    [self.profileImageView addGestureRecognizer:tap];
+    
+    self.profileImageView.clipsToBounds=YES;
+    self.profileImageView.layer.cornerRadius=self.profileImageView.frame.size.width/2;
+}
 
 #pragma mark - UITableViewDataSource
 
@@ -59,6 +75,85 @@
     return 70;
 }
 
+- (void)pickImage {
+    UIAlertController *alert=[UIAlertController alertControllerWithTitle:@"Choose source:"
+                                                                 message:nil
+                                                          preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Photo library"
+                                              style:UIAlertActionStyleDefault
+                                            handler:^(UIAlertAction * _Nonnull action) {
+                                                
+                                                UIImagePickerController *picker=[[UIImagePickerController alloc] init];
+                                                picker.sourceType=UIImagePickerControllerSourceTypePhotoLibrary;
+                                                picker.delegate=self;
+                                                picker.allowsEditing=YES;
+                                                
+                                                [self presentViewController:picker animated:YES completion:nil];
+        
+    }]];
+    
+    
+    
+    
+    
+    if (![UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceRear] || [UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront]) {
+    [alert addAction:[UIAlertAction actionWithTitle:@"Camera"
+                                              style:UIAlertActionStyleDefault
+                                            handler:^(UIAlertAction * _Nonnull action) {
+                                                
+                                                
+                                                    
+                                                    UIImagePickerController *picker=[[UIImagePickerController alloc] init];
+                                                    picker.sourceType=UIImagePickerControllerSourceTypeCamera;
+                                                    picker.delegate=self;
+                                                    picker.allowsEditing=YES;
+                                                    
+                                                    [self presentViewController:picker animated:YES completion:nil];
+                                                
+                                            }]];}
+    
+    
+    
+    
+    
+    
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                              style:UIAlertActionStyleCancel
+                                            handler:^(UIAlertAction * _Nonnull action) {
+                                                
+                                            }]];
+    
+    
+
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+
+
+
+#pragma mark - UIImagePickerControllerDelegate
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    UIImage *image = [info objectForKey:UIImagePickerControllerEditedImage];
+    if (!image) {
+        image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    }
+    
+    self.profileImageView.image=image;
+    
+    
+    //after picking image,close imagePickerController
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
 
